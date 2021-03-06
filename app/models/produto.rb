62bd42f1ec37f9d0c60613076b77ed69696d5422
laisy <laisyferreira2011@gmail.com>
2021-03-05 23:53:20 -0300

    Criando validators de Produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,8 @@
 class Produto < ApplicationRecord
+    validates :descricao, presence: true, format: {with: /[A-Za-z0-9]+/}, length: {maximum: 1000, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
+    validates :quantidade, numericality: { only_integer: true }
+    validates :categoria, presence: true
+    validates :subcategoria, presence: true
+    validates :valor, numericality: true
+    validates :tamanho, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :clientes
+  root 'clientes#index'
   resources :produtos
-  root 'produtos#index'
+  #root 'produtos#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
