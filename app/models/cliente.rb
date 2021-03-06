eb05ad3d438d83d458a86cc266632c6bb3bad192
laisy <laisyferreira2011@gmail.com>
2021-03-05 23:57:32 -0300

    Criando valitadors de Clientes

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,5 @@
 class Cliente < ApplicationRecord
+    validates :cpf, presence: true, length: { is: 11 } , uniqueness: true
+    validates :nome, presence: true, length: {minimum: 3}
+    validates :telefone, numericality: { only_integer: true }
 end
