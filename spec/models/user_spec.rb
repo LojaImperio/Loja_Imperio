66555876a033eb3e23a4c0a4823b3b4e67413986
laisy <https://github.com/laisy>
2021-04-25 14:12:32 -0300

    Testes de model rspec User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper' 
 describe User do 
- it "é válido quando contém o email e senha" 
- it "não é válido quando o email é nulo" 
+    it "é válido quando email e senha estão presentes" do 
+        user = User.new( email: 'bruce@ironmaiden.com', password: '123456') 
+        expect(user).to be_valid 
+    end 
+    it "é invalido sem o email" do 
+        user = User.new(email: nil) 
+        user.valid?   
+        expect(user.errors[:firstname]).to include("can't be blank") 
+    end
+    it "é inválido caso já exista um e-mail igual" do 
+        user = User.new(email: 'bruce@ironmaiden.com', password: '123456') 
+        user = User.new(email: 'bruce@ironmaiden.com', password: '654123') 
+        user.valid? 
+        expect(user.errors[:email]).to include('has already been taken')
+    end 
 end
